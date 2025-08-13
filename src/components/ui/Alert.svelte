06c40ed48b3180597add900eab794db0ffd65b27
Hozaifa Bin Masud <hozaifabinmasud@gmail.com>
2025-08-13 19:14:32 +0600

    Setup alert

---
+++ src/components/ui/Alert.svelte
@@ -0,0 +1,23 @@
+<script>
+	import messageStore from '../../storage/message.store';
+
+	export let type;
+	export let message;
+
+	function closeAlert() {
+		messageStore.hide();
+	}
+</script>
+
+<div class="row mt-3">
+	<div class="col">
+		<div
+			class="alert alert-dismissible {type === 'error' ? 'alert-danger' : 'alert-success'}"
+			role="alert"
+		>
+			<strong>{type === 'error' ? 'Error' : 'Success'}:</strong>
+			{message}
+			<button onclick={closeAlert} type="button" class="btn-close" aria-label="Close"></button>
+		</div>
+	</div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,8 @@
 	import favicon from '$lib/assets/favicon.svg';
 	import 'bootstrap/dist/css/bootstrap.min.css';
 	import Navbar from '../components/Navbar.svelte';
+	import Alert from '../components/ui/Alert.svelte';
+	import messageStore from '../storage/message.store';
 
 	let { children } = $props();
 </script>
@@ -12,5 +14,8 @@
 
 <Navbar />
 <main class="container">
+	{#if $messageStore.show}
+		<Alert message={$messageStore.message} type={$messageStore.type} />
+	{/if}
 	{@render children?.()}
 </main>
